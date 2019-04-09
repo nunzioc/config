b8962abf60494e69fad309881dcb63dd8693164f
nunzio cicone <nunziocicone@gmail.com>
2019-04-08 21:18:06 -0400

    update fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,19 @@
-#! /bin/fish
-function fish_prompt
-	test $SSH_TTY
-    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-    test "$USER" = 'root'
-    and echo (set_color red)"#"
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
 
-    # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color green)'❯ '
+    # PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    __terlar_git_prompt
+    __fish_hg_prompt
+    echo
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    echo -n '➤ '
+    set_color normal
 end
