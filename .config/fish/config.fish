31789e6659156f6ab347194540030852e118b79c
Nunzio Cicone <nunziocicone@localhost.localdomain>
2019-05-04 08:32:23 -0400

    added z.lua bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,3 +33,22 @@ end
 function fk
   kak (fzf --height 50% --reverse)
 end
+
+bind \ck 'fk'
+
+# z.lua
+source (lua ~/source/z.lua/z.lua --init fish enhanced | psub)
+
+function __z.lua_most_recent
+  z -I -t .
+  commandline -f repaint
+end
+
+function __z.lua_subdirectory
+  z -I -c .
+  commandline -f repaint
+end
+
+bind \cf '__z.lua_most_recent'
+bind \cd '__z.lua_subdirectory'
+
