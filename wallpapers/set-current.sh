32552a62a70255af96c1ad9f08bfb3a3c4c083b0
nunzio cicone <nunziocicone@localhost.localdomain>
2020-10-29 14:23:10 -0400

    sync

--- wallpapers/set-current.sh
+++ wallpapers/set-current.sh
@@ -1,6 +1,6 @@
-#!/bin/ksh
-set -A files $(ag -g png --ignore current.png) # create an array of files
-N=${#files[@]}         # Number of members in array
-((N=RANDOM%N))
-cp ${files[$N]} current.png
-feh --bg-fill /home/ncicone/wallpapers/current.png
+#!/bin/bash
+rm $HOME/wallpapers/current.png
+files=(~/wallpapers/*) # create an array of files
+rand=${files[$RANDOM % ${#files[@]}]}
+cp $rand $HOME/wallpapers/current.png
+feh --bg-scale $HOME/wallpapers/current.png
