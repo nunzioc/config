a996f473e00d077dbfce0cc03e4f5575773065b8
Nunzio Cicone <nunziocicone@gmail.com>
2022-02-05 20:11:39 -0500

    post install update

--- .config/post-install.sh
+++ .config/post-install.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
-curl https://sh.rustup.rs -sSf | sh
-$HOME/.cargo/bin/cargo install ripgrep ruplacer skim zoxide fd-find
 
-os=$(lsb_release -d)
-packages="kitty zsh kakoune trash-cli"
+os=$(hostnamectl | grep "Operating System")
+packages="kitty zsh kakoune trash-cli git util-linux-user emacs"
 case $os in
     *Ubuntu*)
         sudo apt install -y $packages ;;
@@ -12,6 +10,27 @@ case $os in
     *)
         echo 'Error: OS $os is not supported' ;;
 esac
+
+git config --global user.email "nunziocicone@gmail.com"
+git config --global user.name "Nunzio Cicone"
+
+ssh-keygen -t ed25519 -C "nunziocicone@gmail.com"
+ssh-add $HOME/.ssh/id_ed25591
+
+echo "Add ~/.ssh/id_ed25591 to github ssh keys. type yes when done."
+read ignore
+
+cd $HOME
+mkdir .configrepo
+git clone --bare https://github.com/nunzioc/config.git ~/.configrepo
+alias config='/usr/bin/git --git-dir=$HOME/.configrepo/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+config checkout
+config submodule update --init --recursive
+
+curl https://sh.rustup.rs -sSf | sh
+$HOME/.cargo/bin/cargo install ripgrep ruplacer skim zoxide fd-find
+
 # change shell to zsh
 chsh -s `which zsh`
 # gnome settings
@@ -36,4 +55,5 @@ $BEGINNING/custom0/ binding "<Super>Return"
 $BEGINNING/custom1/ name "Nautilus"
 $BEGINNING/custom1/ command "/usr/bin/nautilus --new-window"
 $BEGINNING/custom1/ binding "<Super>E"
+
 
