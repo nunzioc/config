c076358b33fed2b3520eb62b000b5fb3f516bf07
Nunzio Cicone <johncicone@northwesternmutual.com>
2023-07-10 14:27:41 -0400

    no wallpapers

--- wallpapers/set-wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-png=($HOME/wallpapers/*.png)
-jpg=($HOME/wallpapers/*.jpg)
-files=("${png[@]}" "${jpg[@]}") # create an array of files
-rand=${files[$RANDOM % ${#files[@]}]}
-feh --bg-scale $rand
