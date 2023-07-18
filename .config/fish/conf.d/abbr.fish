1d101493c135490480c314e7b449a773749e0c08
Nunzio Cicone <johncicone@northwesternmutual.com>
2023-07-18 10:21:22 -0400

    sync

---
+++ .config/fish/conf.d/abbr.fish
@@ -0,0 +1,14 @@
+abbr -a py python
+abbr -a ru ruplacer
+abbr -a icat kitty +kitten icat
+abbr -a gf git fetch -p --tags
+abbr -a gp git pull
+abbr -a gl git log --oneline --graph --decorate -n 15
+abbr -a gc git commit -m
+abbr -a gb git branch -a
+abbr -a ga git add . -v
+abbr -a gd git diff --histogram --word-diff=color
+abbr -a gs git status
+abbr -a gr git rebase
+abbr -a gco git checkout
+abbr -a firefox flatpak run org.mozilla.firefox
