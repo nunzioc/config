1f3c703204f6487b040b784ff14a2e5c36571abc
Nunzio Cicone <johncicone@northwesternmutual.com>
2023-08-08 09:46:43 -0400

    sync

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,14 +1,15 @@
 function fish_prompt
+    echo
     set last_status $status
     if test (hostname) = "toolbox"
         echo -n toolbox:
     end
-    echo -n (prompt_pwd -d 1)
+    echo (prompt_pwd -d 1) (fish_git_prompt)
     if test $last_status -gt 0
         set_color red;
         echo -n " "$last_status
     else
         set_color green;
     end
-    echo -n ' ❯ '; set_color normal
+    echo -n '❯❯ '; set_color normal
 end
---
+++ .config/fish/functions/transient_prompt_func.fish
@@ -0,0 +1,4 @@
+function transient_prompt_func
+    set_color green; echo -n '❯❯ '; set_color normal
+end
+
